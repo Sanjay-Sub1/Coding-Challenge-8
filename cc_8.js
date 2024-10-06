564d020b113146687e598c413203809c9ff1f45a
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-10-06 18:56:08 -0400

    Create Department class to manage employees and calculate salary

--- cc_8.js
+++ cc_8.js
@@ -10,6 +10,25 @@ class Employee{
     getDetails(){console.log(`${this.name},${this.position},${this.salary}`)}
 }
 
+//Task 2 create a class Department that you can add employees to
+class Department{
+    constructor(name){
+        this.name = name;
+        this.employees = [];
+    }
+//Creates a method to add a new employee into the class Department
+addEmployee(employee){
+    const isEmployee = ['name','position','department', 'salary'];
+
+    if (isEmployee){
+        this.employees.push(employee);
+    }
+    else{
+        console.log("Error");
+    }
+}
+}
+
 //Adds 2 new employees into the class
 const employee1 = new Employee('Sanjay', 150000, 'Sales VP', 'Sales');
 const employee2 = new Employee('Sander', 85000, 'Director', 'Marketing');
@@ -17,3 +36,15 @@ const employee2 = new Employee('Sander', 85000, 'Director', 'Marketing');
 //Logs both employees using the getDetails method
 console.log(employee1.getDetails());
 console.log(employee2.getDetails());
+
+//Creating a new department as well as 2 new employees
+const humanResources = new Department('HR');
+const employee3 = new Employee('Charles','Assistant' , 'HR', 80000);
+const employee4 = new Employee('Leila', 'Manager', 'HR', 130000);
+
+//Using the addEmployee method to add our newly created emplyoees into the class
+engineering.addEmployee(employee3);
+engineering.addEmployee(employee4);
+
+//Logs the data for employee3
+console.log(employee3);
