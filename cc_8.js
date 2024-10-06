5efb08a8629e6c461b45b63b3f084ced46bc23b1
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-10-06 18:21:08 -0400

    Create Employee class with properties and methods

--- cc_8.js
+++ cc_8.js
@@ -1,4 +1,4 @@
-//Task 1 creates a class :Employee"
+//Task 1 creates a class :Employee" with a position, department, and salary
 class Employee{
     constructor(name, position, department, salary){
         this.name = name;
@@ -6,11 +6,14 @@ class Employee{
         this.position = position;
         this.salary = salary;
     }
+//Has a getDetails method to grab details of certain employees 
     getDetails(){console.log(`${this.name},${this.position},${this.salary}`)}
 }
 
+//Adds 2 new employees into the class
 const employee1 = new Employee('Sanjay', 150000, 'Sales VP', 'Sales');
 const employee2 = new Employee('Sander', 85000, 'Director', 'Marketing');
 
+//Logs both employees using the getDetails method
 console.log(employee1.getDetails());
-console.log(emp2.getDetails());
+console.log(employee2.getDetails());
